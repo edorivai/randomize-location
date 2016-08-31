a55fc85e66e2e148425b812e52360fc6c7f7fd9d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2016-08-31 18:23:52 +0200

    Improve documentation by renaming one-letter variables

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@
  * @param {Object} options
  * @param {number} options.lat - The latitude of the location that is to be randomized
  * @param {number} options.long - The longitude of the location that is to be randomized
- * @param {number} options.r - The radius in meters within which the randomized location
+ * @param {number} options.radius - The radius in meters within which the randomized location
  *   will be generated.
- * @param {number} [options.u=Math.random()] - The random variable determines the distance
+ * @param {number} [options.spread=Math.random()] - The random variable determines the distance
  *   from the original location
- * @param {number} [options.v=Math.random()] - The random variable that determines
+ * @param {number} [options.angle=Math.random()] - The random variable that determines
  *   the angle relative to the location
+ * @param {number} options.minOffset - The minimal distance the randomized point
+ *   should be from the passed latitude and longitude
  * @returns {Object} object with randomized lat and long
  */
 function randomizeLocation(options) {
@@ -23,16 +25,16 @@ function randomizeLocation(options) {
 		throw new Error('Invalid longitude');
 	}
 
-	var r = (opts.r || 100) / 111300; // Defaults to 100 meters
-	var u = opts.u || Math.random();
-	var v = opts.v || Math.random();
-	var	w = r * Math.sqrt(u);
-	var t = 2 * Math.PI * v;
-	var x = w * Math.cos(t);
-	var y1 = w * Math.sin(t);
-	var x1 = x / Math.cos(opts.lat);
+	var radius = (opts.radius || 100) / 111300; // Defaults to 100 meters
+	var spread = opts.spread || Math.random();
+	var angle = opts.angle || Math.random();
+	var	w = radius * Math.sqrt(spread);
+	var rAngle = 2 * Math.PI * angle;
+	var x = w * Math.cos(rAngle);
+	var ryOffset = w * Math.sin(rAngle);
+	var rxOffset = x / Math.cos(opts.lat);
 
-	return { lat: opts.lat + y1, long: opts.long + x1 };
+	return { lat: opts.lat + ryOffset, long: opts.long + rxOffset };
 }
 
 module.exports = randomizeLocation;
